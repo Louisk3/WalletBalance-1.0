2f337d92ebe414250d3997509b0dba25f5deacf6
LouisK <devdoctor9504@gmail.com>
2024-05-16 12:20:29 +0900

    build the table

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,52 @@
-import { useState } from 'react'
+import { useState, useMemo } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Connection, PublicKey, LAMPORTS_PER_SOL } from '@solana/web3.js';
+import Table from './Table'
+
+const getData = () => [
+  {
+    address: "GTCsHMpJoutAsuMDzr86n3DS7bjB83gAUYYj7XWKZ9sL",
+  },
+  {
+    address: "BX8kfSnDG23P8YkwKNYcn6TTDjGPPfkdG829U23SRL4A",
+  },
+];
 
 function App() {
   const [count, setCount] = useState(0)
 
+  const columns = useMemo(
+    () => [
+      {
+        Header: "Address",
+        accessor: "address"
+      },
+      {
+        Header: "Balance",
+        accessor: "balance"
+      },
+    ],
+    []
+  );
+
+  const data = useMemo(() => getData(), []);
+
+  // const RPC = clusterApiUrl('mainnet-beta')
+  const RPC = 'https://fluent-side-isle.solana-mainnet.quiknode.pro/19a27fc1fa07c0a0aff254ef753b1ba030360b39/'
+  const SOLANA_CONNECTION = new Connection(RPC)
+  const WALLET_ADDRESS = 'BX8kfSnDG23P8YkwKNYcn6TTDjGPPfkdG829U23SRL4A'
+
+  const getBalance = async () => {
+    let balance = await SOLANA_CONNECTION.getBalance(new PublicKey(WALLET_ADDRESS))
+    console.log(`Wallet Balance: ${balance/LAMPORTS_PER_SOL}`)
+    setCount(balance)
+  }
+
   return (
     <>
-      <div>
+      {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -18,8 +56,8 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => getBalance()}>
+          Balance: {count/LAMPORTS_PER_SOL}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
@@ -27,7 +65,20 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
+
+      <div className='App'>
+        <div className='min-h-screen bg-gray-100 text-gray-900'>
+          <main className='max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 pt-4'>
+            <div>
+              <h1 className='text-xl font-semibold'>Filter Balance</h1>
+            </div>
+            <div className='mt-4'>
+              <Table columns={columns} data={data} />
+            </div>
+          </main>
+        </div>
+      </div>
     </>
   )
 }
---
+++ src/Table.jsx
@@ -0,0 +1,99 @@
+import { 
+  useTable,
+  useGlobalFilter,
+  useAsyncDebounce,
+  useFilters,
+  useSortBy,
+  usePagination 
+} from 'react-table'
+
+function Table({ columns, data }) {
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    prepareRow,
+    page,
+    canPreviousPage,
+    canNextPage,
+    pageOptions,
+    pageCount,
+    gotoPage,
+    nextPage,
+    previousPage,
+    setPageSize,
+    state,
+    preGlobalFilteredRows,
+    setGlobalFilter
+  } = useTable(
+    {
+      columns,
+      data
+    },
+    useFilters,
+    useGlobalFilter,
+    useSortBy,
+    usePagination
+  )
+
+  console.log(`page: ${page}`)
+
+  return (
+    <div className="mt-2 flex flex-col">
+      <div className="-my-2 overflow-x-auto -mx-4 sm:-mx-6 lg:-mx-8">
+        <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+          <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+            <table
+              {...getTableProps()}
+              className="min-w-full divide-y divide-gray-200"
+            >
+              <thead className="bg-gray-50">
+                {headerGroups.map((headerGroup) => (
+                  <tr {...headerGroup.getHeaderGroupProps()}>
+                    {headerGroup.headers.map((column) => (
+                      // Add the sorting props to control sorting. For this example
+                      // we can add them into the header props
+                      <th
+                        scope="col"
+                        className="group px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                      >
+                        <div className="flex items-center justify-between">
+                          {column.render("Header")}
+                        </div>
+                      </th>
+                    ))}
+                  </tr>
+                ))}
+              </thead>
+              <tbody
+                {...getTableBodyProps()}
+                className="bg-white divide-y divide-gray-200"
+              >
+                {page.map((row, i) => {
+                  // new
+                  prepareRow(row);
+                  return (
+                    <tr {...row.getRowProps()}>
+                      {row.cells.map((cell) => {
+                        return (
+                          <td
+                            {...cell.getCellProps()}
+                            className="px-6 py-4 whitespace-nowrap"
+                          >
+                            {cell.render("Cell")}
+                          </td>
+                        );
+                      })}
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Table;
